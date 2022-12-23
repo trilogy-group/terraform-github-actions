7b74f1f0d841d81fe1e418bfb19af74619a42801
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-23 12:18:16 +0530

    Debugging

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+raw_recco=${./cloudfix-linter/cloudfix-linter recco}
+echo "${raw_recco}"
+res=${gh api repos/trilogy-group/terraform-github-actions/issues/${{ github.event.pull_request.number }}/comments \
+            -f body="${raw_recco}"}
+echo "${res}"
