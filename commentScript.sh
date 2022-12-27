389f3ccf20cae2341ffb0ed488efee7d0607742d
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-27 18:48:16 +0530

    Removing Extra files

--- commentScript.sh
+++
@@ -1,6 +0,0 @@
-gh api repos/trilogy-group/terraform-github-actions/pulls/${{ github.event.pull_request.number }}/comments \
-  -f body="This Line" \
-  -f path="main.tf" \
-  -F line="1" \
-  -f side="RIGHT" \
-  -f commit_id="${{ github.event.pull_request.head.sha }}"
